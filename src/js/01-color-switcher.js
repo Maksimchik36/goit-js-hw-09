808d8cb18edb670d52760826c7b191df1e64eca1
Maksimchik36 <Peltek1985@gmail.com>
2022-05-11 17:03:14 +0300

    task-1 btn conditions

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,21 +2,22 @@ const btnStart = document.querySelector("button[data-start]");
 const btnStop = document.querySelector("button[data-stop]");
 const bodyRef = document.querySelector("body");
 
+let timerId = null;
 
 btnStart.addEventListener('click', () => {timerId = setInterval(onBtnStartClick, 1000)});
 btnStop.addEventListener('click', onBtnStopClick);
 
 btnStop.disabled = true; // кнопку стоп изначально делаем не активной
 
-let timerId = null;
-
 // Изменение цвета body
 function changeBodyColor() {
     bodyRef.style.backgroundColor = getRandomHexColor();
 }
 
-function onBtnStartClick() {    
-    btnStart.disabled = true;
+function onBtnStartClick() {   
+    if (!btnStart.disabled) {
+        btnStart.disabled = true;
+    }
     if (btnStop.disabled) {
             btnStop.disabled = false;
     }
