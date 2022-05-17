923fbecdbd2f445d36cad58cad91944c87467d53
Maksimchik36 <Peltek1985@gmail.com>
2022-05-17 21:51:30 +0300

    task 2 completed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,84 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+import Notiflix from 'notiflix';
 
+const startBtnRef = document.querySelector("button[data-start]");
+startBtnRef.disabled = true;
 
+const daysRef = document.querySelector("span[data-days]");
+const hoursRef = document.querySelector("span[data-hours]");
+const minutesRef = document.querySelector("span[data-minutes]");
+const secondsRef = document.querySelector("span[data-seconds]");
+const timerRef = document.querySelector(".timer");
+
+// По умолчанию должно быть - "ничто"
+let timerId = null;
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+
+// Опции для библиотеки flatpickr. В методе onClose - персональные настройки для нашего задания
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const userSelectedDate = selectedDates[0].getTime();
+    const currentTime = Date.now();
+    const deltaTime = userSelectedDate - currentTime;
+   
+    if(deltaTime > 0){
+     startBtnRef.disabled = false;
+     } else {
+      Notiflix.Notify.failure("Please choose a date in the future");
+      //  alert("Please choose a date in the future");
+      }
+  },
+};
+
+// Инициализация библиотеки на элементе input#datetime-picker
+const inputData = flatpickr("#datetime-picker", options);
+
+function onStartBtnClick(){
+  if(timerId !== null){
+    return;
+  }
+
+  startBtnRef.disabled = true;
+
+  timerId = setInterval(()=>{
+    const deltaTimeValue = countDeltaTime();
+    if(deltaTimeValue < 0){
+      clearInterval(timerId);
+      Notiflix.Notify.success('Congratulations !!! It’s time for miracles and magic for you !!!');
+      return;
+    }
+
+    const parameterOfTime = convertMs(deltaTimeValue);
+ 
+    visualizeCounter(parameterOfTime); 
+  }
+      , 1000)  
+}
+
+// Рассчитывает и выдает разницу между текущим и выбранным временем
+function countDeltaTime(){
+  const dataFromInput = inputData.selectedDates[0];
+  const currentTime = Date.now(); 
+  const deltaTime = dataFromInput - currentTime;
+  return deltaTime;      
+}
+
+// Записывает время из объекта параметров в счетчик
+function visualizeCounter({days, hours, minutes, seconds}){
+  daysRef.textContent = addLeadingZero(days.toString());
+  hoursRef.textContent = addLeadingZero(hours.toString());
+  minutesRef.textContent = addLeadingZero(minutes.toString());
+  secondsRef.textContent = addLeadingZero(seconds.toString());
+}
+
+// Преобразовывает число в объект параметров и выдает его
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -22,9 +98,7 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-
-// напиши function addLeadingZero(value)  используя   padStart(2, "0")
+function addLeadingZero(value){
+  const rezult = value.padStart(2, "0");
+  return rezult;
+}
