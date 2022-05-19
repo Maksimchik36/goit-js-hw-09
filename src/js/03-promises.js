3563e6707f9457a8a55f1e845627652e587ef16b
Maksimchik36 <Peltek1985@gmail.com>
2022-05-19 14:28:22 +0300

    task-3 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,53 @@
 import Notiflix from 'notiflix';
 
 const formRef = document.querySelector(".form");
-const delayRef = document.querySelector("input[name='delay']");
-const stepRef = document.querySelector("input[name='step']");
-const AmountRef = document.querySelector("input[name='amount']");
 
-formRef.addEventListener("submit", createPromise);
+const inputData = {};
+
+formRef.addEventListener("submit", getDataFromInputs);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  
-  } else {
-    // Reject
 
-  }
+  const promise = new Promise((resolve, reject) => {
+        
+    setTimeout(()=>{
+      if (shouldResolve) {
+        // Fulfill
+        resolve({position, delay});
+      } else {
+        // Reject
+        reject({position, delay});
+      }
+    }, delay)
+  })  
+  return promise;
 }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+function getDataFromInputs(event){
+  event.preventDefault();
+  inputData.amount = Number(event.currentTarget.amount.value);
+  inputData.step = Number(event.currentTarget.step.value);
+  inputData.delay = Number(event.currentTarget.delay.value);
+  abracadabra(inputData);
+}
+
+function abracadabra({amount, step, delay}){
+for(let i = 1; i <= amount; i += 1){
+  let position = i;
+
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delay += step;
+}
+}
+
+
+
+
+
